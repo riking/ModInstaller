9a6f645aa7e9fb377a756d91a8a4b1023ff0b263
riking <rikingcoding@gmail.com>
2011-12-21 15:32:35 -0800

    Completed short function declarations.

--- src/modinstaller/packages/MITertiaryContentHandler.java
+++ src/modinstaller/packages/MITertiaryContentHandler.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package modinstaller.packages;
+import java.util.ArrayList;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.Attributes;
 import org.xml.sax.Locator;
@@ -14,25 +15,42 @@ import org.xml.sax.Locator;
 public class MITertiaryContentHandler implements ContentHandler
 {
     private Mod working;
+    private ArrayList<Mod> modArray;
+    private boolean finished=false;
+    private String debugDesc;
     
+    public MITertiaryContentHandler(String filename)
+    {
+        this.debugDesc=filename;
+    }
+    public MITertiaryContentHandler()
+    {
+        this.debugDesc = "N/A";
+    }
     @Override
     public void startDocument()
     {
+        modArray = new ArrayList<Mod>(10);
     }
     @Override
     public void endDocument()
     {
-        
+        finished=true;
+    }
+    public ArrayList<Mod> getMods()
+    {
+        if(!finished) throw new RuntimeException("Tried to get mod list before parsing complete");
+        return modArray;
     }
     @Override
     public void skippedEntity(String name)
     {
-        
+        java.util.logging.Logger.getLogger("modPackageReader").log(java.util.logging.Level.WARNING, "Skipped entity in file ''{0}'': {1}", new Object[]{debugDesc, name});
     }
     @Override
     public void processingInstruction(String target, String data)
     {
-        
+        java.util.logging.Logger.getLogger("modPackageReader").log(java.util.logging.Level.WARNING, "Unexpected processing instruction in file{0}", debugDesc);
     }
     @Override
     public void startElement(String uri, String localname, String qname, Attributes atts)
@@ -46,19 +64,23 @@ public class MITertiaryContentHandler implements ContentHandler
     }
     @Override
     public void characters(char[] ch, int start, int length) 
-    {}
+    {
+    
+    }
     
     @Override
     public void ignorableWhitespace(char[] ch, int start, int length){}
-    
+    //do nothing.
     @Override
     public void endPrefixMapping(String prefix) 
     {}
+    //do nothing.
     @Override
     public void startPrefixMapping(String prefix, String uri) 
     {}
+    //do nothing.
     @Override
     public void setDocumentLocator(Locator locator)
-    {
-    }
+    {}
+    //do nothing.
 }
