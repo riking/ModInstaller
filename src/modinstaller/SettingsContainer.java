e6fc7374006817ed78cd7a6c17213b24f4ee2acc
Riking <rikingcoding@gmail.com>
2011-12-28 17:14:13 -0800

    Began Settings class

---
+++ src/modinstaller/SettingsContainer.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modinstaller;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.BufferedReader;
+
+/**
+ *
+ * @author kane
+ */
+public class SettingsContainer {
+    static Color colorUpToDate = Color.GREEN.darker();
+    static Color colorMinorUpdate = Color.RED.darker();
+    static Color colorMajorUpdate = Color.YELLOW.darker();
+    
+    private static boolean init;
+    private SettingsContainer inst;
+    private File settingsFile;
+    
+    public SettingsContainer(File location)
+    {
+        settingsFile = location;
+    }
+    
+    public Color getColorUpToDate()
+    {
+        if(!init) init();
+        return colorUpToDate;
+    }
+    public void init()
+    {
+        
+        try
+        {
+            BufferedReader reader = new BufferedReader(new java.io.FileReader(settingsFile));
+        }
+        catch(FileNotFoundException e)
+        {
+            
+        }
+    }
+}
